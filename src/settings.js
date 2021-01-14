5705b7ff7ae904578e97b9b2d600db8b57e80001
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2021-01-14 15:58:52 +0100

    update contract

--- src/settings.js
+++ src/settings.js
@@ -1,10 +1,10 @@
-export const  contractAddress = "KT1J5w67XjzNAo6PX3KQx6XyFApyQq4dC6GN"
+export const  contractAddress = "KT1CxHBiCSvmXn9kXCbhPfdGDAggGG9ktQEX"
 
 export const githubUrl = "https://github.com/edukera/completium-dapp-ideabox"
 
 export const appTitle = "Switch on a Connected Object!"
 
-export const network = "carthagenet"
+export const network = "delphinet"
 
 export const appName = "IotCorp."
 
