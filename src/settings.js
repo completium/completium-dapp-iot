b178ce626299beb380fe63dfc2399199409ab8c4
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2021-04-27 17:11:47 +0200

    update network

--- src/settings.js
+++ src/settings.js
@@ -1,11 +1,11 @@
-export const contractAddress = "KT19ZQUnVrDT5xnfvPqYhn1DeM489875oWGU"
+export const contractAddress = "KT1J62A4AWiC7qKjpTYrqWA6B8cTtcEQzKNS"
 
 export const githubUrl = "https://github.com/edukera/completium-dapp-ideabox"
 
 export const appTitle = "Switch on a Connected Object!"
 
-export const network  = "edo2net"
-export const endpoint = 'https://edonet.smartpy.io'
+export const network  = "florencenet"
+export const endpoint = 'https://florencenet.smartpy.io'
 
 export const appName = "IotCorp."
 
